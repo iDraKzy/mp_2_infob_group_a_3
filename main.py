dea7b45b074e1a76b6ea1b2d9e667d2f17940b5c
iDraKzy <57191896+iDraKzy@users.noreply.github.com>
2020-11-06 12:22:39 +0100

    Data structures and functions

---
+++ main.py
@@ -0,0 +1,48 @@
+def initialize_submarines(nb_submarines):
+    """
+    
+    """
+
+def update_position(submarines_list):
+    """
+    
+    """
+
+def check_submarines_life(submarines_list):
+    """
+    
+    """
+
+def update_target(target_x, target_y):
+    """
+    
+    """
+
+def fire(target_x, target_y):
+    """
+    
+    """
+
+def sonar():
+    """
+    
+    """
+
+
+
+_list = [
+    {
+        'position_x': 1,
+        'position_y': 1,
+        'life': 2,
+        'direction_x': -1,
+        'direction_y': 0
+    },
+    {
+        'position_x': 4,
+        'position_y': 3,
+        'life': 1,
+        'direction_x': 0,
+        'direction_y': 1
+    }
+]
